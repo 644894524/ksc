dc25bfd0df0598ee199dc7efcf38ce6e5e49c09d
sunlong <sunlong@zhangyue.com>
2021-02-13 23:18:15 +0800

    update

---
+++ common/db.go
@@ -0,0 +1,46 @@
+package common
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
+	"time"
+)
+
+var Db *gorm.DB
+
+func InitDb(){
+	drivername := viper.GetString("db.drivername")
+	var args string
+
+	//选择链接的数据库
+	switch drivername {
+	case "mysql":
+		args = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+			viper.GetString("db.dbuser"),
+			viper.GetString("db.dbpwd"),
+			viper.GetString("db.hostname"),
+			viper.GetString("db.port"),
+			viper.GetString("db.dbname"),
+			viper.GetString("db.charset"),
+		)
+
+	default:
+		panic("不支持的数据库")
+	}
+
+	db, err := gorm.Open(drivername, args)
+	if err != nil {
+		panic(err)
+	}
+
+	db.DB().SetConnMaxLifetime(time.Duration(viper.GetInt("db.lifetime")) * time.Second)
+	db.DB().SetMaxOpenConns(viper.GetInt("db.maxconn"))
+	db.DB().SetMaxIdleConns(viper.GetInt("db.idleconn"))
+	Db = db
+	return
+}
+
+func GetDb() *gorm.DB {
+	return Db
+}
---
+++ common/viper.go
@@ -0,0 +1,23 @@
+package common
+
+import (
+	"github.com/spf13/viper"
+)
+
+const (
+	WEB_CONFILE		 = "./config/conf.yaml"
+	WEB_CONFTYPE	 = "yaml"
+)
+
+func InitViper(){
+	viper.SetConfigFile(WEB_CONFILE)
+	viper.SetConfigType(WEB_CONFTYPE)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic("读取配置文件失败")
+	}
+
+	// 监控配置文件变化
+	viper.WatchConfig()
+}
+
