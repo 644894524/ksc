279e23e13e75ee33cf4e4c6232a4c5e4f40cd32a
sunlong <sunlong@zhangyue.com>
2021-02-14 19:21:17 +0800

    update

--- data/article.go
+++ data/article.go
@@ -4,14 +4,14 @@ const tableName = "articles"
 
 //定义模型
 type Article struct {
-	ID          int			`gorm:"column:id"`
-	Name 		string		`gorm:"column:name"`
-	Type		int			`gorm:"column:type;default:0"`
-	Like		int			`gorm:"column:like;default:0"`
-	Collection  int			`gorm:"column:collection;default:0"`
-	CreateTime  int			`gorm:"column:create_time"`
-	UpdateTime	int         `grom:"column:update_time"`
-	Status		int			`gorm:"column:status;default:0"`
+	ID          int			`gorm:"column:id" json:"id"`
+	Name 		string		`gorm:"column:name" json:"name"`
+	Type		int			`gorm:"column:type;default:0" json:"type"`
+	Like		int			`gorm:"column:like;default:0" json:"like"`
+	Collection  int			`gorm:"column:collection;default:0" json:"collection"`
+	CreateTime  int			`gorm:"column:create_time" json:"create_time"`
+	UpdateTime	int         `grom:"column:update_time" json:"update_time"`
+	Status		int			`gorm:"column:status;default:0" json:"status"`
 }
 
 //表名
