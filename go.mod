71d3f9526d9853e0973963ab492aa205cdd8422d
sunlong <sunlong@zhangyue.com>
2021-02-17 20:10:30 +0800

    update

--- go.mod
+++ go.mod
@@ -3,13 +3,20 @@ module ksc
 go 1.15
 
 require (
+	github.com/antchfx/htmlquery v1.2.3 // indirect
+	github.com/antchfx/xmlquery v1.3.4 // indirect
 	github.com/dvwright/xss-mw v0.0.0-20191029162136-7a0dab86d8f6
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-contrib/pprof v1.3.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/microcosm-cc/bluemonday v1.0.4 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/spf13/viper v1.7.1
+	github.com/temoto/robotstxt v1.1.1 // indirect
 	gorm.io/gorm v1.20.12 // indirect
 )
--- script/colly/main.go
+++ script/colly/main.go
@@ -1,7 +1,91 @@
 package main
 
+import (
+	"fmt"
+	"github.com/gocolly/colly"
+	"time"
+)
 
 
 func main(){
+	fmt.Println("start")
 
+	url := "http://www.enjoybar.com"
+
+	//配置
+	agent := colly.UserAgent("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36")
+	depth := colly.MaxDepth(1)
+
+	//首页爬取
+	index := colly.NewCollector(agent, depth)
+
+	//内容页爬取
+	con := index.Clone()
+
+	//请求前调用
+	index.OnRequest(func(r *colly.Request) {
+		fmt.Println("index爬取：", r.URL)
+	})
+
+	//请求发生错误时调用
+	index.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	//采集器，获取首页文章列表
+	index.OnHTML("div[class='container-bd']", func(e *colly.HTMLElement) {
+		csign := 0
+		e.ForEach("div[class='wrap']", func(ci int, cv *colly.HTMLElement){
+			cName := cv.ChildText("dt[class='title'] h3 span a")
+			if cName != "" {
+				csign++
+				cv.ForEach("li", func(itemI int, itemV *colly.HTMLElement) {
+					itemHref := itemV.ChildAttr("a[class='meiwen']", "href")
+					itemName := itemV.ChildText("a[class='meiwen']")
+					if itemName != "" && itemHref != ""{
+						fmt.Println(itemHref, itemName, csign)
+						conUrl := fmt.Sprintf("%s%s", url, itemHref)
+						fmt.Println(conUrl)
+						con.Request("GET", conUrl, nil, nil, nil)
+					}
+				})
+			}
+		})
+	})
+
+	//限速
+	con.Limit(&colly.LimitRule{
+		DomainRegexp: "",
+		DomainGlob:   "*.enjoybar.com/*",
+		Delay:        2 * time.Second,
+		RandomDelay:  0,
+		Parallelism:  1,
+	})
+
+	//(内容)请求前调用
+	con.OnRequest(func(r *colly.Request) {
+		fmt.Println("con爬取：", r.URL)
+	})
+
+	//(内容)错误请求调用
+	con.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	//(内容)
+	con.OnHTML("div[class='article']", func(art *colly.HTMLElement){
+		conDate  := art.ChildText("li[class='pubdate'] span")
+		conClick := art.ChildText("li[class='click'] span")
+		conImage := art.ChildAttr("div[class='arcpic'] img", "src")
+		conArtic := art.ChildText("div[class='text'] p")
+		conTitle := art.ChildText("div[class='article'] h1")
+		fmt.Println(conTitle, conClick, conImage, conArtic, conDate)
+	})
+
+	err := index.Visit(url)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("End")
 }
